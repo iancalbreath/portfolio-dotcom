5d525e90777efb37ad51b04471742e0ab2e459f5
ian c <149852385+iancalbreath@users.noreply.github.com>
2024-04-22 17:23:40 -0700

    Update pdfViewer.js
    I think this link was broken

--- js/pdfViewer.js
+++ js/pdfViewer.js
@@ -1,7 +1,7 @@
 //js to load PDF
         // PDF.js load and display
         const pdfViewer = document.getElementById('pdf-viewer');
-        const pdfUrl = 'https://iancalbreath.com/ICalbreathPro.pdf';
+        const pdfUrl = 'https://iancalbreath.com/resource/ICalbreathPro.pdf';
         // Initialize PDF.js
         pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
             pdf.getPage(1).then(page => {
